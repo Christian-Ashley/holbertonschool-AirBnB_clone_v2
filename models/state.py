dd244f628fa0bf10eef304a9e5ef2a36c0de60c9
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-11-13 23:03:21 +0000

    checking state for task 7

--- models/state.py
+++ models/state.py
@@ -18,3 +18,14 @@ class State(BaseModel, Base):
     else:
         name = ""
 	cites = []
+
+     def cities(self):
+            """
+                getter method, returns list of City objs from storage
+                linked to the current State
+            """
+            city_list = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
