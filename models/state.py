f5866e1ad0c548b8c124a9ddb95a84451f556280
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-11-15 02:07:47 +0000

    more indentation stuff

--- models/state.py
+++ models/state.py
@@ -20,12 +20,12 @@ class State(BaseModel, Base):
 	cites = []
 
     def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+        """
+            getter method, returns list of City objs from storage
+            linked to the current State
+        """
+        city_list = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
